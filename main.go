dc889707dd59a994a567f26078ef27e9725604f0
Bruno Barros <brunobarros2093@gmail.com>
2024-03-24 20:17:59 -0300

    WIP abstract factory

--- main.go
+++ main.go
@@ -1,5 +1,54 @@
 package main
-import "fmt"	
-func main(){
-	fmt.Println("Hello World!")
+
+import (
+	"fmt"
+
+	abstractfactory "github.com/brunobarros2093/golang-in-design-patterns.git/abstractFactory"
+)
+
+type ISportsFactory interface {
+	makeShoe() abstractfactory.IShoe
+	makeShirt() abstractfactory.IShirt
+}
+
+func GetSportsFactory(brand string) (ISportsFactory, error) {
+	if brand == "adidas" {
+		return &abstractfactory.Adidas, nil
+	}
+
+	if brand == "nike" {
+		return &Nike{}, nil
+	}
+
+	return nil, fmt.Errorf("Wrong brand type passed")
+}
+func main() {
+	adidasFactory, _ := GetSportsFactory("adidas")
+	nikeFactory, _ := GetSportsFactory("nike")
+
+	nikeShoe := nikeFactory.makeShoe()
+	nikeShirt := nikeFactory.makeShirt()
+
+	adidasShoe := adidasFactory.makeShoe()
+	adidasShirt := adidasFactory.makeShirt()
+
+	printShoeDetails(nikeShoe)
+	printShirtDetails(nikeShirt)
+
+	printShoeDetails(adidasShoe)
+	printShirtDetails(adidasShirt)
+}
+
+func printShoeDetails(s abstractfactory.Shoe) {
+	fmt.Printf("Logo: %s", s.getLogo())
+	fmt.Println()
+	fmt.Printf("Size: %d", s.getSize())
+	fmt.Println()
+}
+
+func printShirtDetails(s IShirt) {
+	fmt.Printf("Logo: %s", s.getLogo())
+	fmt.Println()
+	fmt.Printf("Size: %d", s.getSize())
+	fmt.Println()
 }
